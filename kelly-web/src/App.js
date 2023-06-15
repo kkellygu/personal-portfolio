6340044615df8b4dca8ec8378275a927d5751526
kkellygu <moonrivergu@gmail.com>
2023-06-15 00:46:15 -0400

    created section folders

--- kelly-web/src/App.js
+++ kelly-web/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Kelly's portfolio website... under construction. Come back soon!
+          Kelly's portfolio website under construction... come back soon!
         </p>
         <a
           className="App-link"
