47b977c0ac596fa21af0d78349455cd193c231e3
kkellygu <moonrivergu@gmail.com>
2023-06-18 01:09:32 -0400

    worked on topbar header of the page -- added number and email, as well as hamburger style menu

--- kelly-web/src/components/topbar/Topbar.jsx
+++ kelly-web/src/components/topbar/Topbar.jsx
@@ -1,4 +1,7 @@
 import "./topbar.css"
+import { FaPhone } from 'react-icons/fa';
+import { MdEmail } from 'react-icons/md';
+
 
 export default function Topbar() {
     return(
@@ -7,11 +10,20 @@ export default function Topbar() {
                 <div className="left">
                     <a href="#home" className="logo">kellz.rgdance</a>
                     <div className="itemContainer">
+                        <FaPhone className="icon" />
+                        <span>703 984 9533</span>
                     </div>
-                   
-                
+                    <div className="itemContainer">
+                        <MdEmail className="icon" />
+                        <span>kkellygu1006@gmail.com</span>
+                    </div>
                 </div>
                 <div className="right"></div>
+                    <div className="hamburger">
+                        <span className="line1"></span>
+                        <span className="line2"></span>
+                        <span className="line3"></span>
+                    </div>
             </div>
         </div>
     )
