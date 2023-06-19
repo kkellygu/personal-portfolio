6e2f3461043d2366324f38bc4b239eb0e6615382
kkellygu <moonrivergu@gmail.com>
2023-06-19 13:12:46 -0400

    added headshot to intro page

--- kelly-web/src/components/intro/Intro.jsx
+++ kelly-web/src/components/intro/Intro.jsx
@@ -1,9 +1,16 @@
 import "./intro.css"
+import React from 'react';
+import kgHeadshot from '/Users/kellygu/workspace/personal-portfolio/kelly-web/src/images/headshot.png';
 
 export default function Intro(){
     return(
         <div className="intro" id="intro">
-        
+            <div className="left"></div>
+            <div className="right">
+                <img src={kgHeadshot} className="imgContainer" alt="Kelly Img" />
+                
+            </div>
+                
         </div>
     )
 }
