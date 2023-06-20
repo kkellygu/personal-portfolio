c5efe92637f6f5f102e9a016ed24c17383e118b0
kkellygu <moonrivergu@gmail.com>
2023-06-20 00:21:31 -0400

    implemented dynamic active class for the boxes that show different aspects of each extracurricular category; react dynamic data with useEffect hook

--- kelly-web/src/components/personal/Personal.jsx
+++ kelly-web/src/components/personal/Personal.jsx
@@ -1,31 +1,43 @@
 import "./personal.css"
-import standin from '/Users/kellygu/workspace/personal-portfolio/kelly-web/src/assets/images/IMG_9660.JPG';
-import {  useState } from "react";
+import {  useState, useEffect } from "react";
+import { csaPage, akdphiPage, guzhengPage, dancePage, photographyPage} from "../../data.js"
 
 export default function Personal(){
     const [selected, setSelected] =useState("CSA")
+    const [data, setData] = useState([])
     const list = [
-        {
-            id: "CSA",
-            title: "csa",
-        },
-        {
-            id: "AKDPHI",
-            title: "aKDPhi",
-        },
-        {
-            id: "GUZHENG",
-            title: "guzheng",
-        },
-        {
-            id: "DANCE",
-            title: "dance",
-        },
-        {
-            id: "PHOTOGRAPHY",
-            title: "photography",
-        },
+        { id: "CSA", title: "csa", },
+
+        { id: "AKDPHI", title: "aKDPhi", },
+
+        { id: "GUZHENG", title: "guzheng", },
+
+        { id: "DANCE", title: "dance", },
+
+        { id: "PHOTOGRAPHY", title: "photography", },
     ];
+
+    useEffect(()=>{
+        switch(selected){
+            case "CSA":
+                setData(csaPage);
+                break;
+                case "AKDPHI":
+                setData(akdphiPage);
+                break;
+                case "GUZHENG":
+                setData(guzhengPage);
+                break;
+                case "DANCE":
+                setData(dancePage);
+                break;
+                case "PHOTOGRAPHY":
+                setData(photographyPage);
+                break;
+                default:
+                setData(csaPage);
+        }
+    },[selected])
 
     return(
         <div className="personal" id="personal">
@@ -39,30 +51,12 @@ export default function Personal(){
             </ul>
             
             <div className="container">
+                {data.map((d) => (
                 <div className="item">
-                    <img src={standin} alt="whoop" />
-                    <h3>presidency</h3>
+                    <img src={d.img} alt="whoop" />
+                    <h3>{d.title}</h3>
                 </div>
-                <div className="item">
-                    <img src={standin} alt="whoop" />
-                    <h3>presidency</h3>
-                </div>
-                <div className="item">
-                    <img src={standin} alt="whoop" />
-                    <h3>presidency</h3>
-                </div>
-                <div className="item">
-                    <img src={standin} alt="whoop" />
-                    <h3>presidency</h3>
-                </div>
-                <div className="item">
-                    <img src={standin} alt="whoop" />
-                    <h3>presidency</h3>
-                </div>
-                <div className="item">
-                    <img src={standin} alt="whoop" />
-                    <h3>presidency</h3>
-                </div>
+                ))}
             </div>
         </div>
     )
